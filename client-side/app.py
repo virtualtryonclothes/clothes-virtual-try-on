7b8051df8863bbce8126a0edf4b48f74c9f7e35c
nileshsinghx <singhnilesh2603@gmail.com>
2024-11-20 13:05:36 +0530

    Update app.py

--- client-side/app.py
+++ client-side/app.py
@@ -18,7 +18,7 @@ def submit():
     model = request.files['model']
 
     ## replace the url from the ngrok url provided on the notebook on server.
-    url = "http://b89a-34-79-184-67.ngrok.io/api/transform"
+    url = "http://127.0.0.1.ngrok.io/api/transform"
     print("sending")
     response = requests.post(url=url, files={"cloth":cloth.stream, "model":model.stream})
     op = Image.open(BytesIO(response.content))
